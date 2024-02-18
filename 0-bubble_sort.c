30af745d78f470af4efa0f52c24b03d1d01e505f
iCelf08 <chfirstly@gmail.com>
2024-02-18 18:08:50 +0100

    Bub

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,18 +14,18 @@ int swip;
 	for (i = 0; i < size - 1; i++)
 	{
 		swip = 0;
-		for (j = size - 1; j > i; j--)
+		for (j = 1; j <= size - 1; j++)
 		{
 			if (array[j] < array[j - 1])
 			{
 				temp = array[j - 1];
 				array[j - 1] = array[j];
 				array[j] = temp;
+				swip = 1;
+				print_array(array, size);
 			}
-			swip = 1;
-			print_array(array, size);
 		}
 	if (swip == 0)
-		break;
+	break;
 	}
 }
