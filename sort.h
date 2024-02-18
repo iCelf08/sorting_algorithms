60ba0ccd946a710154b8af47538cb2fa58c4f54b
iCelf08 <chfirstly@gmail.com>
2024-02-18 19:54:33 +0100

    1

--- sort.h
+++ sort.h
@@ -1,19 +1,15 @@
-#include <stdlib.h>
-#include <stdio.h>
 #ifndef SORT_H
 #define SORT_H
 
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int array[], size_t size);
-
-
 
 #endif
