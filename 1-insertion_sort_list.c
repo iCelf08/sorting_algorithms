c42738bf56b1333876d93138c1e7da493d8c9119
iCelf08 <chfirstly@gmail.com>
2024-02-19 21:42:27 +0100

    1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,1 +1,48 @@
-#ind
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * swap - function to swap nodes in the linked list
+ * @i: first node
+ * @j: second node
+*/
+void swap(listint_t *i, listint_t *j)
+{
+    if (j->next)
+    j->next->prev = i;
+    if (i->prev)
+    i->prev->next = j;
+    i->next = j->next;
+    j->prev = i->prev;
+    i->prev = j;
+    j->next = i;
+}
+/**
+ * insertion_sort_list - function sorts list elements using the insertion sort algorithm
+ * @list: list to be sorted
+ */
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *current, *next;
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
+    current = (*list)->next;
+    while (current != NULL)
+    {
+        next = current;
+        current = current->next;
+        while (next && next->prev)
+        {
+            if (next->n < next->prev->n)
+            {
+                swap(next->prev, next);
+                if (next->prev == NULL)
+                    *list = next;
+                print_list(*list);
+            }
+            else
+            next = next->prev;
+         }
+    }
+}
--- sort.h
+++ sort.h
@@ -11,5 +11,7 @@ struct listint_s *next;
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int array[], size_t size);
+void insertion_sort_list(listint_t **list);
+void swap(listint_t *i, listint_t *j);
 
 #endif
