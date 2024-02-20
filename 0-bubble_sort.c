e6e1430c82d9a43d8047fcbdaac971e01520582a
iCelf08 <chfirstly@gmail.com>
2024-02-20 20:33:56 +0100

    Update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,22 +11,23 @@ void bubble_sort(int array[], size_t size)
 	size_t i, j;
 	int temp;
 	int swip;
-
-	for (i = 0; i < size - 1; i++)
+if (array == NULL)
+return;
+swip = 0;
+for (i = 0; i < size; i++)
+{
+for (j = 1; j <= size - 1; j++)
 	{
-		swip = 0;
-		for (j = 1; j <= size - 1; j++)
+		if (array[j] < array[j - 1])
 		{
-			if (array[j] < array[j - 1])
-			{
-				temp = array[j - 1];
-				array[j - 1] = array[j];
-				array[j] = temp;
-				swip = 1;
-				print_array(array, size);
-			}
+			temp = array[j - 1];
+			array[j - 1] = array[j];
+			array[j] = temp;
+			swip = 1;
+			print_array(array, size);
 		}
-		if (swip == 0)
-			break;
+	}
+	if (swip == 0)
+	break;
 	}
 }
